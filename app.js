fe5ee294c54066a6f905ded9c1b223d218347022
Benjamin B. Huang <benjamin.b.huang@icloud.com>
2017-08-07 22:34:05 -0400

    add post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/libraryApp');
 var Book = require('./models/bookModel');
@@ -8,50 +9,18 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-var bookRouter = express.Router();
+var bookRouter = require('./Routes/bookRoutes')(Book);
 
-bookRouter.route('/Books')
-    .get(function (req, res) {
-        var query = {};
-
-        if(req.query.genre){
-            query.genre = req.query.genre;
-        }
-
-        if(req.query.author){
-            query.author = req.query.author;
-        }
-
-        Book.find(query, function (err, books) {
-            if(err)
-                console.log(err);
-            else
-                res.json(books);
-        });
-        
-
-    });
-
-bookRouter.route('/Books/:bookId')
-    .get(function (req, res) {
-
-        Book.findById(req.params.bookId, function (err, book) {
-            if(err)
-                console.log(err);
-            else
-                res.json(book);
-        });
-
-
-    });
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
+app.use('/api/authors', bookRouter);
 
 app.get('/', function (req, res) {
     res.send('welcome to my API');
 });
 
 app.listen(port, function () {
-console.log('Gulp is running my app on on PORT: '+ port);
+    console.log('Gulp is running my app on on PORT: ' + port);
 });
